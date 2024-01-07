9f7a2d25c167450627a999f68087c3927dcdb437
kuerim <kuerim@taltech.ee>
2024-01-07 21:04:20 +0200

    add week7 code

---
+++ Test7/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.WriteLine("Enter 1, if you want to conver Fahrenheit to Celsius or 2 if you want to convert Celsius to Fahrenheit.");
+int userChoice = int.Parse(Console.ReadLine());
+if (userChoice == 1 || userChoice == 2)
+{
+    Console.WriteLine("What temperature do you want to convert?");
+    double userInput = double.Parse(Console.ReadLine());
+    double result = ConvertToCelsius(userInput);
+    Console.WriteLine($"{userInput} Fahrenheit is {result} Celsius.");
+}
+else
+{
+    Console.WriteLine("Wrong input!");
+}
+
+
+static double ConvertToCelsius(double userInput)
+{
+    return (userInput - 32) * 5 / 9;
+}
+
+static double ConvertToFahrenheit(double userInput)
+{
+    return (userInput * 9) / 5 + 32;
+}
